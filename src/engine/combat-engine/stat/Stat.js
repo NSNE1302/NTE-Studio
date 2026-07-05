bceac4932365c6ba5318ad7c5b21243cc6cd1be2
NSNE1302 <nsne1302@gmail.com>
2026-07-05 17:58:35 +0800

    feat(combat): add stat model

---
+++ src/engine/combat-engine/stat/Stat.js
@@ -0,0 +1,37 @@
+/**
+ * Stat
+ * ------------------------------------------------------
+ * 儲存角色最終面板數值。
+ */
+
+export default class Stat {
+
+    constructor() {
+
+        this.hp = 0;
+
+        this.attack = 0;
+
+        this.defense = 0;
+
+        this.critRate = 0;
+
+        this.critDamage = 0;
+
+        this.damageBonus = 0;
+
+        this.attributeBonus = 0;
+
+        this.penetration = 0;
+
+        this.defenseReduction = 0;
+
+        this.resistanceReduction = 0;
+
+        this.fusionPower = 0;
+
+        this.collapsePower = 0;
+
+    }
+
+}
