50a5d33e70f33cd08072efdf980331d46fbd6b66
NSNE1302 <nsne1302@gmail.com>
2026-07-07 13:28:29 +0800

    feat(combat): add damage result model

---
+++ src/engine/combat-engine/result/DamageResult.js
@@ -0,0 +1,31 @@
+/**
+ * DamageResult
+ * ----------------------------------------------------
+ * 儲存一次傷害計算結果。
+ */
+
+export default class DamageResult {
+
+    constructor() {
+
+        this.finalDamage = 0;
+
+        this.baseArea = 0;
+
+        this.multiplierArea = 0;
+
+        this.defenseArea = 0;
+
+        this.criticalArea = 0;
+
+        this.damageArea = 0;
+
+        this.resistanceArea = 0;
+
+        this.vulnerabilityArea = 0;
+
+        this.fusionArea = 0;
+
+    }
+
+}
